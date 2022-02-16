3677f83538c153890aecec67a0dcba78d2294219
harshrathaur <55021326+harshrathaur@users.noreply.github.com>
2022-02-16 09:35:48 +0530

    Project created

---
+++ src/main/java/com/example/e_healthcare_management/EHealthcareManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_healthcare_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthcareManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHealthcareManagementApplication.class, args);
+    }
+
+}
